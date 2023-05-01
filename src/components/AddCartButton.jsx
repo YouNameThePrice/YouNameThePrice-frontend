c8bf75b02021281b776336e74dbd77f02ba6b32b
Berkan Serbes <berkan_serbes@hotmail.com>
2023-05-01 13:33:49 +0300

    Sepete Ekle butonunun tasarımı oluşturuldu

---
+++ src/components/AddCartButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AddCartButton() {
+  return (
+    <button className="bg-light--gray py-2 px-4 rounded-full border-2 border-black w-48">
+      Sepete Ekle
+    </button>
+  );
+}
+
+export default AddCartButton;
