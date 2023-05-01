bbbef71427b312090e45da125245cf335d290de7
Berkan Serbes <berkan_serbes@hotmail.com>
2023-05-01 11:10:34 +0300

    Ana kategori navigasyon tasarımı eklendi

---
+++ src/components/MainCategory.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function MainCategory() {
+  return (
+    <div className="bg-light--gray border-b border-b-slate-400 py-4">
+      <ul className="flex justify-center space-x-20">
+        <li className="px-2">Teknoloji</li>
+        <li className="px-2">Moda</li>
+        <li className="px-2">Kozmetik</li>
+        <li className="px-2">Kırtasiye</li>
+        <li className="px-2">Spor</li>
+        <li className="px-2">Ev &amp; Yaşam</li>
+        <li className="px-2">Müzik Aletleri</li>
+        <li className="px-2">Süpermarket</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MainCategory;
