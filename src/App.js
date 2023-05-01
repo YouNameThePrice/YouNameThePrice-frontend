b6c10cd98eaa76719c0501933622952d45312780
Berkan Serbes <berkan_serbes@hotmail.com>
2023-05-01 13:37:54 +0300

    index.css ve App.js dosyalarında düzenlemeler yapıldı

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeNavbar from "./components/HomeNavbar";
+import MainCategory from "./components/MainCategory";
+import ProductCard from "./components/ProductCard";
 
+function App() {
+  return (
+    <>
+      <HomeNavbar />
+      <MainCategory />
+      <ProductCard />
+    </>
+  );
+}
+
+export default App;
+
+/* 
 function App() {
   return (
     <BrowserRouter>
@@ -15,4 +31,4 @@ function App() {
   );
 }
 
-export default App;
+*/
