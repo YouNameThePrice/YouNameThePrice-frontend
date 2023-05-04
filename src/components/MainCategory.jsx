36740b48e9d9c31711b79fed8c92710cf13f780f
Berkan Serbes <berkan_serbes@hotmail.com>
2023-05-04 16:06:38 +0300

    Rendering main categories with dynamic way using map func

--- src/components/MainCategory.jsx
+++ src/components/MainCategory.jsx
@@ -1,17 +1,13 @@
 import React from "react";
+import category from "../json/category.json";
 
 function MainCategory() {
   return (
     <div className="bg-light--gray border-b border-b-slate-400 py-4">
       <ul className="flex justify-center space-x-20">
-        <li>Teknoloji</li>
-        <li>Moda</li>
-        <li>Kozmetik</li>
-        <li>Kırtasiye</li>
-        <li>Spor</li>
-        <li>Ev &amp; Yaşam</li>
-        <li>Müzik Aletleri</li>
-        <li>Süpermarket</li>
+        {category.map((item) => (
+          <li>{item.name}</li>
+        ))}
       </ul>
     </div>
   );
