92dfb5c626179aa84889fdd486dc545ffc4aa577
Berkan Serbes <berkan_serbes@hotmail.com>
2023-05-16 18:51:35 +0300

    Home sayfası eklendi

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import HomeNavbar from '../components/HomeNavbar';
+// import MainCategory from '../components/MainCategory';
+import ProductCard from '../components/ProductCard';
+
+function Home() {
+  return (
+    <>
+      {/* <HomeNavbar /> */}
+      {/* <MainCategory /> */}
+      <ProductCard />
+    </>
+  )
+}
+
+export default Home
