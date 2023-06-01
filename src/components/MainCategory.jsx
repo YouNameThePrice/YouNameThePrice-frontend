20e8be4ebe09a82f3df35d41405de8b6548bc341
Berkan Serbes <berkan_serbes@hotmail.com>
2023-06-01 22:43:10 +0300

    Modified MainCategory.jsx

--- src/components/MainCategory.jsx
+++ src/components/MainCategory.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import category from "../json/category.json";
+import categories from "../json/category.json";
+import { Link } from "react-router-dom";
 
 function MainCategory() {
   return (
     <div className="bg-light--gray border-b border-b-slate-400 py-4">
       <ul className="flex justify-center space-x-20">
-        {category.map((item) => (
-          <li>{item.name}</li>
+        {categories.map((category) => (
+          <li key={category.name}><Link to={`/${category.name.toLocaleLowerCase()}`}>{category.name}</Link></li>
         ))}
+        
       </ul>
     </div>
   );
