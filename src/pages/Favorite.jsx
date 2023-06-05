cd220429e8aab257f34de0372017e3d3e7485e85
Berkan Serbes <berkan_serbes@hotmail.com>
2023-06-05 11:24:45 +0300

    Added Favorite page

---
+++ src/pages/Favorite.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ProfileSidebar from "../components/ProfileSidebar";
+
+function Favorite() {
+  return (
+    <div className="flex flex-row  h-[83.2vh] max-h-screen">
+      <ProfileSidebar />
+
+      {/* Favorilerim */}
+      <div></div>
+    </div>
+  );
+}
+
+export default Favorite;
