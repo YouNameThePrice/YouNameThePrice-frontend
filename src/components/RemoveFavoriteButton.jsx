88024beb3038fe892b5d7644a80152451de7c2ea
Berkan Serbes <berkan_serbes@hotmail.com>
2023-06-06 08:58:35 +0300

    RemoveFavoriteButton component added

---
+++ src/components/RemoveFavoriteButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RemoveFavoriteButton() {
+  return (
+    <button className="bg-orange py-2  rounded-full border-2 border-black w-48">
+      Favorilerden Çıkar
+    </button>
+  );
+}
+
+export default RemoveFavoriteButton;
