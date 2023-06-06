3936e1106b9201c6cda5d8dc9e0c8a1b0bd16e21
Berkan Serbes <berkan_serbes@hotmail.com>
2023-06-06 09:00:03 +0300

    Modified Favorite page

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ProfileSidebar from "../components/ProfileSidebar";
+import FavoriteCard from "../components/FavoriteCard";
 
 function Favorite() {
   return (
     <div className="flex flex-row  h-[83.2vh] max-h-screen">
       <ProfileSidebar />
 
-      {/* Favorilerim */}
-      <div></div>
+      {/* Favorite products container */}
+      <div className="flex flex-col space-y-4 p-8 overflow-y-scroll w-full">
+        {/* FavoriteCard */}
+        <FavoriteCard />
+      </div>
     </div>
   );
 }
