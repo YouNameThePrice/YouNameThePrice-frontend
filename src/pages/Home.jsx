675dd7b870760ca685bc79e726f8776c48509cb1
Berkan Serbes <berkan_serbes@hotmail.com>
2023-06-07 20:30:51 +0300

    Added filtering products according to their categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import Subcategory from "../components/Subcategory";
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import product from "../json/data.json";
 
 function Home() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedSubCategory, setSelectedSubCategory] = useState("");
   const location = useLocation();
   const showSubcategory = location.pathname !== "/";
+  const { category, subcategory } = useParams();
+
+  useEffect(() => {
+    setSelectedCategory(category);
+    setSelectedSubCategory(subcategory);
+  }, [category, subcategory]);
+
+  const filteredProducts = product.filter((product) => {
+    if (selectedSubCategory) {
+      return (
+        product.subCategory.toLowerCase() === selectedSubCategory.toLowerCase()
+      );
+    } else if (selectedCategory) {
+      return product.category.toLowerCase() === selectedCategory.toLowerCase();
+    }
+    return true;
+  });
 
   return (
     <div className="flex h-[83.2vh] max-h-screen">
       {showSubcategory && <Subcategory />}
 
-      <div className="flex flex-wrap overflow-y-scroll">
-        {product.map((product) => (
+      <div className="flex flex-wrap w-full overflow-y-scroll">
+        {filteredProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
